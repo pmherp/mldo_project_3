e512414b5c927c026e526e2369699edefec41f29
Philip Herp <philipherp@MBP-von-Philip.fritz.box>
2023-02-10 08:12:36 +0100

    changed test_model.py after data cleanup; adjusted for remote pytests

--- starter/starter/test_model.py
+++ starter/starter/test_model.py
@@ -8,18 +8,29 @@ from sklearn.model_selection import train_test_split
 
 @pytest.fixture
 def data():
-    data = pd.read_csv('../data/census.csv')
+    try:
+        data = pd.read_csv('../data/census.csv')
+    except:
+        data = pd.read_csv('../../starter/data/census.csv')
+
     y = data['salary']
+
     X = data.drop(['salary'], axis=1)
+
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42)
+
     return X_train, X_test, y_train, y_test
 
 
 def test_train_model(data):
     X_train, _, y_train, _ = data
-    with open("../model/model.pkl", "rb") as model_file:
-        clf = pickle.load(model_file)
+    try:
+        with open("../model/model.pkl", "rb") as model_file:
+            clf = pickle.load(model_file)
+    except:
+        with open("../../starter/model/model.pkl", "rb") as model_file:
+            clf = pickle.load(model_file)
 
     assert clf.classes_ is not None, 'Classes not found'
     assert clf.n_classes_ == len(
@@ -28,35 +39,37 @@ def test_train_model(data):
 
 def test_model_parameters(data):
     X_train, _, y_train, _ = data
-    with open("../model/model.pkl", "rb") as model_file:
-        clf = pickle.load(model_file)
+    try:
+        with open("../model/model.pkl", "rb") as model_file:
+            clf = pickle.load(model_file)
+    except:
+        with open("../../starter/model/model.pkl", "rb") as model_file:
+            clf = pickle.load(model_file)
 
     assert clf.n_estimators == 100, 'Incorrect number of trees'
 
 
 def test_data_types():
-    data = pd.read_csv('../data/census.csv')
+    try:
+        data = pd.read_csv('../data/census.csv')
+    except:
+        data = pd.read_csv('../../starter/data/census.csv')
 
     expected_types = {
-        "Unnamed: 0.4": int,
-        "Unnamed: 0.3": int,
-        "Unnamed: 0.2": int,
-        "Unnamed: 0.1": int,
-        "Unnamed: 0": int,
         "age": int,
         "workclass": object,
         "fnlgt": int,
         "education": object,
-        "education-num": int,
-        "marital-status": object,
+        "education_num": int,
+        "marital_status": object,
         "occupation": object,
         "relationship": object,
         "race": object,
         "sex": object,
-        "capital-gain": int,
-        "capital-loss": int,
-        "hours-per-week": int,
-        "native-country": object,
+        "capital_gain": int,
+        "capital_loss": int,
+        "hours_per_week": int,
+        "native_country": object,
         "salary": object
     }
 
